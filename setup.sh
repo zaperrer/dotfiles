ad4f2f25f488ca7e0a0aec5bdb7d39246bcdfc63
charlie <charlie_depalmo@apple.com>
2024-09-08 15:17:12 -0700

    🎨formatting setup

--- setup.sh
+++ setup.sh
@@ -1,35 +1,30 @@
 #!/usr/bin/env bash
 current_dir=$PWD
-cd ~/.dotfiles
+cd ~/.dotfiles || exit
 
 # Install programs
-echo -e "\nInstalling Brew and programs..."
-if ! command -v brew &> /dev/null
-then
-    sudo -u $SUDO_USER /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+    sudo -u "$SUDO_USER" /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 for program in \
-  git \
-  stow \
-  raycast \
-  font-fira-code-nerd-font \
-  font-hack-nerd-font \
-  ripgrep \
-  thefuck \
-  bat \
-  fzf \
-  eza \
-  pyenv \
-  ; do
-  brew list $program &>/dev/null || brew install $program
+    git \
+    stow \
+    raycast \
+    font-fira-code-nerd-font \
+    font-hack-nerd-font \
+    ripgrep \
+    thefuck \
+    bat \
+    fzf \
+    eza \
+    pyenv; do
+    brew list $program &>/dev/null || brew install $program
 done
 
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
 
-echo -e "\nStowing apps for user: ${whoami}"
 stow -v .
-echo -e "\n##### ALL DONE"
-cd $current_dir # Return to previous directory
+cd "$current_dir" || exit # Return to previous directory
